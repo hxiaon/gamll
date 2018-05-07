76470624c6a263e6945b9c7e436814068330fea2
xiaoning <xncocoon@foxmail.com>
2018-05-07 08:46:03 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/gmall/gmall_1129/Gmall1129Application.java
@@ -0,0 +1,12 @@
+package com.atguigu.gmall.gmall_1129;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gmall1129Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gmall1129Application.class, args);
+	}
+}
